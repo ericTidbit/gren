f2baeefc6c56a47c43c930b2689155485c84d21c
ericTidbit <2467672@cegepoutaouais.qc.ca>
2025-02-20 11:32:41 -0500

    Add Grenouille methods

--- Grenouille.py
+++ Grenouille.py
@@ -4,28 +4,51 @@ class Grenouille:
     """
     Représente une grenouille
     """
-    def __init__(self, longueur_langue: float, taille: int, couleur: str = "vert", longueur_saut: int = 100, position: int = 0):
+    def __init__(self, longueur_langue: float, taille: int, poids: float, longueur_saut: int, couleur: str = "vert", position: int = 0):
         self.longueur_langue = longueur_langue
-        self.taille = taille
         self.longueur_saut = longueur_saut
 
+        self.poids = poids
+
+        self._taille = None
+        self.set_taille(taille)
+
         self._couleur = couleur
-        set_couleur(couleur)
 
         self._position = None
-        self._position = position
+        self.set_position(position)
 
     # getters
+    def get_poids(self) -> float:
+        return self.poids
+
+    def get_taille(self) -> int:
+        return self._taille
+
+    def get_couleur(self) -> str:
+        return self._couleur
+
     def get_position(self) -> int:
         return self._position
 
     # setters
-    def set_couleur(self, couleur: str) -> None:
-        if couleur.lower() in ["vert", "rouge", "bleu", "jaune", "mauve"]
-            self._couleur = couleur
+    def set_poids(self, poids: float) -> None:
+        if poids > 0:
+            self.poids = poids
+        else:
+            raise ValueError("Poids doit être positif et plus grand que 0")
+
+    def set_taille(self, taille: int) -> None:
+        if taille >= 1:
+            self._taille = taille
+        else:
+            raise ValueError("La taille de la grenouille doit être plus grande ou égale à 1 cm")
 
     def set_position(self, position: int) -> None:
-        self._position = position
+        if position >= 0:
+            self._position = position
+        else:
+            raise ValueError("La position doit être positive")
 
     # methods
     def sauter(self, impulsion: int) -> None:
@@ -33,7 +56,10 @@ class Grenouille:
         :param impulsion: Valeur entre 0 et 100, que détermine le pourcentage du saut
         :return: rien
         """
-        pass
+        if 0 <= impulsion <= 100:
+            self._position = self.longueur_saut * impulsion / 100
+        else:
+            raise ValueError("Impulsion doit être entre 0 et 100")
 
     def manger(self, mouche: Mouche) -> None:
         """
@@ -42,7 +68,14 @@ class Grenouille:
         :param mouche: la mouche a manger
         :return: Rien
         """
-        pass
+        if abs(self._position - mouche.get_position()) <= self.longueur_langue:
+            self.set_poids(self.poids + mouche.get_poids())
+            print("La grenouille mange la mouche!")
+        else:
+            print("La grenouille ne mange pas la mouche!")
 
     def parler(self) -> None:
         print("Ribbit !")
+
+    def __str__(self):
+        return f"Une grenouille de {self.poids} grammes et {self._taille} cm."
--- Mouche.py
+++ Mouche.py
@@ -1,9 +1,22 @@
 class Mouche:
-    def __init__(self, position: int = 0):
-        self._position = position
+    def __init__(self, poids: int, position: int = 0):
+        self._poids = None
+        self.set_poids(poids)
+
+        self._position = None
+        self.set_position(position)
+
+    def get_poids(self):
+        return self._poids
 
     def get_position(self) -> int:
         return self._position
+
+    def set_poids(self, poids: int):
+        if poids >= 0:
+            self._poids = poids
+        else:
+            raise ValueError("Poids doit être plus grand que 0")
 
     def set_position(self, new_position):
         if new_position >= 0:
